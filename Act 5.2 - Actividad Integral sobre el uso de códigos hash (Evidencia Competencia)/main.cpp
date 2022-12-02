b8aad972b20c26263857a3f4c41c67edad24385e
AVA758 <89211436+AVA758@users.noreply.github.com>
2022-12-02 17:31:49 -0600

    Add files via upload

---
+++ Act 5.2 - Actividad Integral sobre el uso de códigos hash (Evidencia Competencia)/main.cpp
@@ -0,0 +1,43 @@
+// =================================================================
+//
+// File: main.cpp
+// Author: Ricardo Navarro Gómez - A01708825
+//		   Alain Vicencio Arizabalo - A01620758
+//		   Fernando Josue Matute Soto - A00833375
+// Date:
+//
+// =================================================================
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  int m, n, value; 
+  string word, p;
+  map<string, int>dictionary; 
+  
+  
+  cin >> m; 
+  cin >> n; 
+
+  // Dictionary creation.
+  for(int i  = 0; i < m; i++) {
+    cin >> word; 
+    cin >> value;
+    dictionary.insert(pair<string, int>(word, value));  
+  }
+
+  // Word reading and salary calculation. 
+  for(int i = 0; i < n; i++) {
+    int salary = 0;
+    while(true) {
+      cin >> p; 
+      if (p == ".") {
+        break;
+      }
+      salary += dictionary[p]; 
+    } cout << salary << endl;
+	}
+	return 0;
+}
